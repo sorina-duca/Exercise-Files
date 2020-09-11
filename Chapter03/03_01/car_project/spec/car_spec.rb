927ad4cdc579dca174f74e9a6bc18c2c91436f78
Sorina Duca <sorina.duca@gmail.com>
2020-09-11 13:50:57 +0200

    create tests

---
+++ Chapter03/03_01/car_project/spec/car_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'car'
+
+describe 'Car' do
+  describe 'attributes' do
+    it 'allows reading and writing for :make' do
+      car = Car.new
+      car.make = 'Dacia'
+      expect(car.make).to eq('Dacia')
+    end
+  end
+
+  describe '.colors' do
+    it 'returns an array of colors' do
+      colors = ['blue', 'black', 'red', 'green']
+      expect(Car.colors).to match_array(colors)
+    end
+  end
+
+  describe '#full_name' do
+    context 'when initialized with no arguments' do
+      it 'returns a string with default values' do
+        expect(Car.new.full_name).to eq('2007 Volvo (unknown)')
+      end
+    end
+  end
+end
