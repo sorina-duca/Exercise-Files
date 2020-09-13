909f4c2dbd196a7ac62364a389b492dddd9521c9
Sorina Duca <sorina.duca@gmail.com>
2020-09-13 13:46:19 +0200

    refactor using let method

--- Chapter03/03_01/car_project/spec/car_spec.rb
+++ Chapter03/03_01/car_project/spec/car_spec.rb
@@ -2,15 +2,17 @@
 
 require 'car'
 
-before(:example) do
-  @car = Car.new
-end
+# before(:example) do
+#   @car = Car.new
+# end
+
+let(:car) {Car.new}
 
 describe 'Car' do
   describe 'attributes' do
     it 'allows reading and writing for :make' do
-      @car.make = 'Dacia'
-      expect(@car.make).to eq('Dacia')
+      car.make = 'Dacia'
+      expect(car.make).to eq('Dacia')
     end
 
     it 'allows writing for :doors'
@@ -26,7 +28,7 @@ describe 'Car' do
   describe '#full_name' do
     context 'when initialized with no arguments' do
       it 'returns a string with default values' do
-        expect(@car.full_name).to eq('2007 Volvo (unknown)')
+        expect(car.full_name).to eq('2007 Volvo (unknown)')
       end
     end
   end
