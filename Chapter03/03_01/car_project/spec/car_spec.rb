6f7994753cce854ba18f83fa3ae181a7d20ab848
Sorina Duca <sorina.duca@gmail.com>
2020-09-13 13:58:55 +0200

    refactor using describe and classname

--- Chapter03/03_01/car_project/spec/car_spec.rb
+++ Chapter03/03_01/car_project/spec/car_spec.rb
@@ -5,14 +5,12 @@ require 'car'
 # before(:example) do
 #   @car = Car.new
 # end
-
-let(:car) {Car.new}
-
-describe 'Car' do
+describe Car do
+  # let(:car) {Car.new}
   describe 'attributes' do
     it 'allows reading and writing for :make' do
-      car.make = 'Dacia'
-      expect(car.make).to eq('Dacia')
+      subject.make = 'Dacia'
+      expect(subject.make).to eq('Dacia')
     end
 
     it 'allows writing for :doors'
@@ -28,7 +26,7 @@ describe 'Car' do
   describe '#full_name' do
     context 'when initialized with no arguments' do
       it 'returns a string with default values' do
-        expect(car.full_name).to eq('2007 Volvo (unknown)')
+        expect(subject.full_name).to eq('2007 Volvo (unknown)')
       end
     end
   end
